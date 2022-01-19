36ff1f34c9fa8b0184f492d3304bb5227f9a7adb
Nurillo Abdikodirov <59973492+NAbdikodirov@users.noreply.github.com>
2022-01-19 01:18:56 -0600

    Update file.js

--- file.js
+++ file.js
@@ -5,5 +5,25 @@ window.onload = function(){
     btn.onclick = 
         function(){
             count += 1;
+            y -= 25;
+            context.clearRect(0,0,600,400);
+
+            context.beginPath();
+            context.arc(x,y,50,0,2 * Math.PI);
+            context.fillStyle="red";
+            context.fill();
+
+            context.font = '25px Arial';
+            context.fillStyle = 'white';
+            context.fillText("Count: " + count, 20, 30);
         }
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var x = 300;
+var y = 350;
+
+context.arc(x, y, 50, 0, 2 * Math.PI):
+context.fillStyle="red";
+context.fill();       
 }
