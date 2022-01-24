e53acbcec4f83f1e2422fc18e336392df1bbad2e
Nurillo Abdikodirov <59973492+NAbdikodirov@users.noreply.github.com>
2022-01-23 23:52:03 -0600

    Update file.js

--- file.js
+++ file.js
@@ -6,6 +6,7 @@ window.onload = function(){
         function(){
             count += 1;
             y -= 25;
+    function draw() {       
             context.clearRect(0,0,600,400);
 
             context.beginPath();
@@ -16,7 +17,10 @@ window.onload = function(){
             context.font = '25px Arial';
             context.fillStyle = 'white';
             context.fillText("Count: " + count, 20, 30);
+
+            window.requestAnimationFrame(draw);
         }
+    }
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
@@ -25,5 +29,28 @@ var y = 350;
 
 context.arc(x, y, 50, 0, 2 * Math.PI):
 context.fillStyle="red";
-context.fill();       
+context.fill();
+
+document.onkeydown = function(){
+    count += 1;
+    y -= 25;
+}
+
+document.ontouchstart = function(){
+    count += 1;
+    y -= 25;
+}
+
+var timePassed = (Date.now() - t) / 1000;
+t = Date.now();
+
+if(y <= 350){
+    speed += 50 * timePassed;
+    y += speed*timePassed;
+}
+
+if(y>350){
+    count = 0
+}
+
 }
